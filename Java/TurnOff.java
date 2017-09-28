8d2ad85594eafef89f461a01efd657653541a0c7
Jaber Shamali <jaber_sha@hotmail.com>
2017-09-28 13:11:20 -0300

    Caga ai Yudi
    Metodo Frete OK
    Metodo Distancia e Centro de Distribuição mais próximo OK
    Em andamento : Adição de camada de exceção

--- Java/TurnOff.java
+++ Java/TurnOff.java
@@ -1,5 +1,8 @@
 package br.com.fiap.nac;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TurnOff {
@@ -14,7 +17,10 @@ public class TurnOff {
 		double []vl_total= new double[3];
 		double soma=0, peso_total=0;
 		int[] qtd_prod= new int [3];
-		double dist=0;
+		boolean resp=false;
+		NumberFormat df = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		DecimalFormat pf= new DecimalFormat("0.000");
+		
 
 		Scanner entrada = new Scanner(System.in);		
 
@@ -36,45 +42,137 @@ public class TurnOff {
 		// Pedido
 		System.out.println("Nome do cliente: ");
 		String cliente = entrada.next();
+		
+		
 		//Pedido: Quantidade por modelo
+		do{
+		try{
+			
+		
+		//Laço para obter quantos beancos de cada cor serão "comprados"
 		for(int i=0;i<3;++i){
 			System.out.println("Quantidade do "+beacon_nome[i]);
 			qtd_prod[i]=entrada.nextInt();	
 			vl_total[i]=qtd_prod[i]*beacon_prec_vol_pes[i][0];
-			System.out.println(vl_total[i]);
+			System.out.println(df.format(vl_total[i]));
+			
+			
+			
+			//Somo para obter o preço totaol de beacons Solicitados
 			soma=vl_total[i]+soma;
+			
+			
+			
+			
+			//Soma peso total do pedido
 			peso_total=peso_total+(qtd_prod[i]*beacon_prec_vol_pes[i][2]);
-			double frete = valorFrete(peso_total,dist);
 		}
-		//Teste de dados do pedido(Quantidade solicitado por produto, valor total dos produtos e peso total)
-		System.out.println("Nome "+cliente);
+		}catch(Exception e){
+			System.out.println(getExcecao(e));
+		
+		// Valida se existe produto no pedido
+		if(soma>0){
+		
+		//Instancia o método para retornar a distancia e Centro de distribuição mais próximo
+		double dist=centroMaisProximo(cd_lt_lg,cd_nome);
+		System.out.println("Distância em km: "+Math.round(dist));
+		
+		
+		
+		//Instancia o método para retornar o valor do frete
+		double frete = valorFrete(peso_total,dist);
+		
+		
+		
+		
+			
+
+		//Teste de dados do pedido(Quantidade solicitado por produto, valor total dos produtos e peso total)	
+		System.out.println("===========Nota Fiscal===========");
+		System.out.println("Cliente: "+cliente);
+		System.out.println("CPF/CNPJ: XXXXXXXXXX-XX");
 		System.out.println("Quantidade "+beacon_nome[0]+": "+qtd_prod[0]);
 		System.out.println("Quantidade "+beacon_nome[1]+": "+qtd_prod[1]);	
 		System.out.println("Quantidade "+beacon_nome[2]+": "+qtd_prod[2]);	
-		System.out.println("Valor total do pedido: "+soma+" R$");
-		System.out.println("Peso total do pedidot: "+peso_total+" Kg");
+		System.out.println("Valor total do pedido: "+df.format(soma)+" R$");
+		System.out.println("Peso total do pedido:"+pf.format(peso_total)+" Kg");
+		System.out.println("Valor do frete: "+df.format(frete));
+		
+		}else if(soma<1){
+			System.out.println("Pedido não existe produto(s)");
+			
+			
+			
+			//Valida se quer tentar novamente
+			System.out.println("Deseja tentar novamente?\n S = Sim \n N = Não");
+			char op = entrada.next().toUpperCase().charAt(0);
+			if(op=='S'){
+				resp=true;
+			}if(op=='N'){
+				resp=false;
+			}
+		}}finally{
+			System.out.println("Deseja tentar novamente?\n S = Sim \n N = Não");
+		char op = entrada.next().toUpperCase().charAt(0);
+		if(op=='S'){
+			resp=true;
+		}if(op=='N'){
+			resp=false;
+			
+		}
+			
+		}
+		}while(resp==true);
+	}
 
+	//Metodo para retornar o centro de distribuição mais próximo
+	public static double centroMaisProximo(double cd_lt_lg[][],String cd_nome[] ){
+		double dist[]= new double[3];
+		double aux;
+		String auxcidade;
+		
+		
+		
+		//Faz a conta entre "CD" de Destino
+		for(int i=0;i<3;++i){
+			dist[i]= (Math.sqrt((-5.4742-cd_lt_lg[i][0])*(-5.4742-cd_lt_lg[i][0]) +(-35.124-cd_lt_lg[i][1])*(-35.124-cd_lt_lg[i][1])))*109;
+			System.out.println("CD "+cd_nome[i]+"\nDistância: "+Math.round(dist[i])+" Km");
+		}
+		
+		
+		
+		//Faz a ordenação e retorna o "CD" mais próximo do Destino
+		for(int i=0;i<2;++i){
+			if(dist[i]>dist[i+1]){
+				auxcidade=cd_nome[i+1];
+				cd_nome[i+1]=cd_nome[i];
+				cd_nome[i]=auxcidade;
+				aux=dist[i+1];
+				dist[i+1]=dist[i];
+				dist[i]=aux;
+
+			}
+
+
+		}
+		System.out.println("Centro de Distribuição mais próximo: "+cd_nome[0]);
+		return dist[0];
 	}
-	
-	//Metodo para retornar a distância 
-	public static double distancia(){
-		return 0;
-	}
+
 
 	// Metodo para retornar valor do Frete
 	public static double valorFrete(double peso_total, double dist){
 		float fixo;
 		float transp;
-		double trecho;
-
+		double trecho,frete=0;
+		
 		if (dist >= 500)
 		{
 			transp = 0.20F;
 			fixo = 50.0F;
 			trecho = dist / 300;
 
-			System.out.println("Valor total do Frete");
-			System.out.println(peso_total *(Math.round(trecho) * transp) + fixo);
+			frete=(peso_total *(Math.round(trecho) * transp) + fixo);
 		}
 		else if (dist < 500 && dist >= 100)
 		{
@@ -82,8 +180,7 @@ public class TurnOff {
 			fixo = 30.0F;
 			trecho = dist / 50;
 
-			System.out.println("Valor total do Frete");
-			System.out.println(peso_total *(Math.round(trecho) * transp) + fixo);
+			frete=(peso_total *(Math.round(trecho) * transp) + fixo);
 
 		}
 		else if (dist < 100)
@@ -92,10 +189,19 @@ public class TurnOff {
 			fixo = 10.0F;
 			trecho = dist / 25;
 
-			System.out.println("Valor total do frete é");
-			System.out.println(peso_total *(Math.round(trecho) * transp) + fixo);
-			}
-		return 0;
+			frete=(peso_total *(Math.round(trecho) * transp) + fixo);
+		}
+		return frete;
+	}
+	
+	public static String getExcecao(Exception e){
+		if (e instanceof java.util.InputMismatchException){
+			return "Valor digitado inválido";
+		}else if (e instanceof NumberFormatException){
+			return "Número inválido";
+		}else{
+			return "Erro desconhecido";
 		}
 	}
-
+	
+}
