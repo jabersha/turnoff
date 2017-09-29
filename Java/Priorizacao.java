d94b79915ca22e39635941d26530d82f49eeefb8
Vitor <vtorzy@gmail.com>
2017-09-29 18:05:45 -0300

    Update Priorizacao.java

--- Java/Priorizacao.java
+++ Java/Priorizacao.java
@@ -7,7 +7,7 @@
  * da verificação realizada, se o pedido for FRETE ele vai para a matriz priorizado, se nao
  * ele vai para a matriz naoPriorizados. No final é exibido os produtos que são priorizados e não.
  */
-public class Priorizacao {
+public class Priorizar4 {
 	public static void main(String[] args) {
 
 		// VALORES CHUMBADOS
@@ -26,7 +26,7 @@ public class Priorizacao {
 
 			//	Caso o pedido tenha prioridade ao frete
 			if (s[x][2].equals("frete")) {
-				priorizados[iPrior] = s[x];	//	Atribui o conteudo á matriz de prioridade
+				priorizados[iPrior] = s[x];	//	Atribui o conteudo a matriz de prioridade
 				iPrior++;	//	Aumenta o tamanho da matriz de pedidos priorizados
 				
 			} else {	//	Se ter prioridade á prazo
@@ -40,7 +40,7 @@ public class Priorizacao {
 		System.out.println("NÃO PRIOZADOS: ");
 		
 		for (int x = 0; x < iNPrior; x++) {	// Varre a matriz baseado no tamanho das incrementacoes
-			for (int y = 0; y < s[x].length; y++) {	//	Varre a matriz baseado no tamanho das colunas dos valores chumbados, pode colocar como 2/3 aki
+			for (int y = 0; y < naoPriorizados[x].length; y++) {//	Varre a matriz baseado no tamanho das colunas dos valores chumbados, pode colocar como 2/3 aki
 				System.out.print(naoPriorizados[x][y] + " | ");
 			}
 			System.out.println();
@@ -48,7 +48,7 @@ public class Priorizacao {
 		System.out.println();
 		System.out.println("PRIORIZADOS: ");
 		for (int x = 0; x < iPrior; x++) {
-			for (int y = 0; y < s[x].length; y++) {
+			for (int y = 0; y < priorizados[x].length; y++) {
 				System.out.print(priorizados[x][y] + " | ");
 			}
 			System.out.println();
