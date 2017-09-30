e255ed6886f7f26c509d4b85154dd491083a059a
Jaber Shamali <jaber_sha@hotmail.com>
2017-09-30 14:20:08 -0300

    Quase lá 1.0
    Adicionado:
    Método para prazo
    Método para produção(que retorna prazo extra para produção e caso seja necessário comprar materia prima também)

--- Java/TurnOff.java
+++ Java/TurnOff.java
@@ -20,11 +20,16 @@ public class TurnOff {
 		double []vl_total= new double[3];
 		double soma=0, peso_total=0;
 		int[] qtd_prod= new int [3];
+		int qtd_prod_producao=0;
 		int[] qtd_prod_estoque= {50,50,50};
+		int qtd_mp= 100;
+		boolean produzir=false;
+		int prazo_prod=0;
+		int prazo=0;
 		boolean resp=false;
 		NumberFormat df = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
 		DecimalFormat pf= new DecimalFormat("0.000");
-		
+
 
 		Scanner entrada = new Scanner(System.in);		
 
@@ -46,7 +51,7 @@ public class TurnOff {
 		// Pedido
 		System.out.println("Escolha o cliente que irá realizar o pedido \nOpções: \n 0 - T-System \n 1 - FIAP \n 2 - 7Comm");
 		int cliopt =entrada.nextInt();
-		
+
 		if(cliopt==0){
 			cli=cliente[0][0];
 		}if(cliopt==1){
@@ -54,97 +59,115 @@ public class TurnOff {
 		}if(cliopt==2){
 			cli=cliente[2][0];
 		}
-		
+
 		//Pedido: Quantidade por modelo
 		do{
 
-			
-		
-		//Laço para obter quantos beancos de cada cor serão "comprados"
-		for(int i=0;i<3;++i){
-			System.out.println("Quantidade do "+beacon_nome[i]);
-			qtd_prod[i]=entrada.nextInt();	
-			vl_total[i]=qtd_prod[i]*beacon_prec_vol_pes[i][0];
-			System.out.println(df.format(vl_total[i]));
-			
-			
-			
-			//Somo para obter o preço totaol de beacons Solicitados
-			soma=vl_total[i]+soma;
-			
-			
-			
-			
-			//Soma peso total do pedido
-			peso_total=peso_total+(qtd_prod[i]*beacon_prec_vol_pes[i][2]);
-		}
-		
-		
-		
-		
-		
-		// Valida se existe produto no pedido
-		if(soma>0){
-		
-		//Instancia o método para retornar a distancia e Centro de distribuição mais próximo
-		double dist=centroMaisProximo(cliopt,cli_lt_lg,cd_lt_lg,cd_nome);
-		System.out.println("Distância em km: "+Math.round(dist));
-		
-		
-		
-		//Instancia o método para retornar o valor do frete
-		double frete = valorFrete(peso_total,dist);
-		
-		
-		
-		
-			
 
-		//Teste de dados do pedido(Quantidade solicitado por produto, valor total dos produtos e peso total)	
-		System.out.println("===========Nota Fiscal===========");
-		System.out.println("Cliente: "+cli);
-		System.out.println("CPF/CNPJ: XXXXXXXXXX-XX");
-		System.out.println("Quantidade "+beacon_nome[0]+": "+qtd_prod[0]);
-		System.out.println("Quantidade "+beacon_nome[1]+": "+qtd_prod[1]);	
-		System.out.println("Quantidade "+beacon_nome[2]+": "+qtd_prod[2]);	
-		System.out.println("Valor total do pedido: "+df.format(soma)+" R$");
-		System.out.println("Peso total do pedido:"+pf.format(peso_total)+" Kg");
-		System.out.println("Valor do frete: "+df.format(frete));
-		
-		}else if(soma<1){
-			System.out.println("Pedido não existe produto(s)");
-			
-			
-			
-			//Valida se quer tentar novamente
-			System.out.println("Deseja tentar novamente?\n S = Sim \n N = Não");
-			char op = entrada.next().toUpperCase().charAt(0);
-			if(op=='S'){
-				resp=true;
-			}if(op=='N'){
-				resp=false;
+
+			//Laço para obter quantos beancos de cada cor serão "comprados"
+			for(int i=0;i<3;++i){
+				System.out.println("Quantidade do "+beacon_nome[i]);
+				qtd_prod[i]=entrada.nextInt();	
+				vl_total[i]=qtd_prod[i]*beacon_prec_vol_pes[i][0];
+				System.out.println(df.format(vl_total[i]));
+
+
+
+				//Somo para obter o preço totaol de beacons Solicitados
+				soma=vl_total[i]+soma;
+
+
+
+
+				//Soma peso total do pedido
+				peso_total=peso_total+(qtd_prod[i]*beacon_prec_vol_pes[i][2]);
+
+				//Condição para soma de fabricação 
+				if(qtd_prod[i]>50) {
+					qtd_prod_producao=qtd_prod[i]-50+qtd_prod_producao;
+					produzir=true;
+				}
 			}
-		}
+
+
+
+
+
+
+
+			// Valida se existe produto no pedido
+			if(soma>0){
+
+				//Instancia o método para retornar a distancia e Centro de distribuição mais próximo
+				double dist=centroMaisProximo(cliopt,cli_lt_lg,cd_lt_lg,cd_nome);
+				System.out.println("Distância em km: "+Math.round(dist));
+				
+				prazo =prazoCDCli(dist,prazo);
+				System.out.println("Prazo sem produção: "+prazo);
+				//Condição para acionar metodo de produção
+				
+				if(produzir==true) {
+					prazo_prod=producao(qtd_prod_producao,qtd_mp,prazo);
+					prazoFabCD(dist);
+					System.out.println("Prazo de producao: "+prazo_prod);
+				}
+
+				
+
+				//Instancia o método para retornar o valor do frete
+				double frete = valorFrete(peso_total,dist);
+
+
+
+
+
+
+				//Teste de dados do pedido(Quantidade solicitado por produto, valor total dos produtos e peso total)	
+				System.out.println("===========Nota Fiscal===========");
+				System.out.println("Cliente: "+cli);
+				System.out.println("CPF/CNPJ: XXXXXXXXXX-XX");
+				System.out.println("Quantidade "+beacon_nome[0]+": "+qtd_prod[0]);
+				System.out.println("Quantidade "+beacon_nome[1]+": "+qtd_prod[1]);	
+				System.out.println("Quantidade "+beacon_nome[2]+": "+qtd_prod[2]);	
+				System.out.println("Valor total do pedido: "+df.format(soma)+" R$");
+				System.out.println("Peso total do pedido:"+pf.format(peso_total)+" Kg");
+				System.out.println("Valor do frete: "+df.format(frete));
+
+			}else if(soma<1){
+				System.out.println("Pedido não existe produto(s)");
+
+
+
+				//Valida se quer tentar novamente
+				System.out.println("Deseja tentar novamente?\n S = Sim \n N = Não");
+				char op = entrada.next().toUpperCase().charAt(0);
+				if(op=='S'){
+					resp=true;
+				}if(op=='N'){
+					resp=false;
+				}
+			}
 		}while(resp==true);
 	}
-	
-	
+
+
 	//Metodo para retornar o centro de distribuição mais próximo
 	public static double centroMaisProximo(int cliopt, double cli_lt_lg[][],double cd_lt_lg[][],String cd_nome[] ){
 		double dist[]= new double[3];
-		double aux;
-		String auxcidade;
-		
-		
-		
+		double aux=0;
+		String auxcidade="";
+
+
+
 		//Faz a conta entre "CD" e Destino
 		for(int i=0;i<3;++i){
 			dist[i]= (Math.sqrt((cli_lt_lg[cliopt][0]-cd_lt_lg[i][0])*(cli_lt_lg[cliopt][0]-cd_lt_lg[i][0]) +(cli_lt_lg[cliopt][1]-cd_lt_lg[i][1])*(cli_lt_lg[cliopt][1]-cd_lt_lg[i][1])))*109;
 			System.out.println("CD "+cd_nome[i]+"\nDistância: "+Math.round(dist[i])+" Km");
 		}
-		
-		
-		
+
+
+
 		//Faz a ordenação e retorna o "CD" mais próximo do Destino
 		for(int i=0;i<2;++i){
 			if(dist[i]>dist[i+1]){
@@ -154,13 +177,12 @@ public class TurnOff {
 				aux=dist[i+1];
 				dist[i+1]=dist[i];
 				dist[i]=aux;
-
 			}
 
 
 		}
-		System.out.println("Centro de Distribuição mais próximo: "+cd_nome[0]);
-		return dist[0];
+		System.out.println("Centro de Distribuição mais próximo: "+auxcidade);
+		return aux;
 	}
 
 
@@ -169,7 +191,7 @@ public class TurnOff {
 		float fixo;
 		float transp;
 		double trecho,frete=0;
-		
+
 		if (dist >= 500)
 		{
 			transp = 0.20F;
@@ -197,7 +219,7 @@ public class TurnOff {
 		}
 		return frete;
 	}
-	
+
 	public static String getExcecao(Exception e){
 		if (e instanceof java.util.InputMismatchException){
 			return "Valor digitado inválido";
@@ -207,5 +229,62 @@ public class TurnOff {
 			return "Erro desconhecido";
 		}
 	}
-	
+	// Metodo que adiciona os dias para produzir os beacons faltantes do pedido
+	public static int producao(int qtd_prod_producao,int qtd_mp,int prazo) {
+		double prazo_prod=0;
+		double	prazo_mp=0;
+		
+		//Caso seja necessário produzir no máximo 50 beacons, que é o limite de fabricação por dia
+		if(qtd_prod_producao<=50){
+
+
+
+
+			//necessário 3 materias primas para produzir 1 beacon
+			//Caso falte materia prima para produzir ele fica negativo e entra na condição
+			prazo_mp=100-(qtd_prod_producao*3);
+			if(prazo_mp<0) {
+
+
+
+				//A materia prima demora 2 dias para cada 80 itens que são pedidos para fornecer
+				//caso passe de 80 soma +2 dias para cada pedido entre 80
+				prazo_mp=Math.ceil(((prazo_mp)*-1)/80)*2;
+				prazo_prod=1+prazo+prazo_mp;	
+			}else {
+				prazo_prod=1+prazo;
+			}
+		//Caso seja necessário produzir mais que o limite diário de 50 beacons
+		}else if(qtd_prod_producao>50){
+			prazo_mp=100-(qtd_prod_producao*3);
+			if(prazo_mp<0) {
+				prazo_mp=Math.ceil(((prazo_mp)*-1)/80)*2;
+				prazo_prod=Math.ceil(qtd_prod_producao/50)+prazo+prazo_mp;
+			}else {
+			prazo_prod=Math.round(qtd_prod_producao)+prazo;	
+			}
+			
+
+		}
+
+		return (int) prazo_prod;
+	}
+	// Metodo para calcular o prazo entre CD e Destino(Cliente) 
+	//sem necessidade de produçao(movimentação entre fábrica e CD)
+	public static int prazoCDCli(double dist,int prazo){
+		if(dist<=100) {
+			prazo=2;
+		}else if(dist>100) {
+			prazo=(int)(Math.ceil(dist/100)*2);
+		}
+		
+		return prazo;
+	}
+	public static int prazoFabCD(double dist) {
+		int prazo_fab=0;
+		
+		
+		return 1;
+	}
+
 }
