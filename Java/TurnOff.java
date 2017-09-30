b8ae3dc4daa29a9a653241743d2ca11a8ac1e583
Jaber Shamali <jaber_sha@hotmail.com>
2017-09-30 05:39:02 -0300

    Quase lá
    Adicionado Clientes com dados chumbados

--- Java/TurnOff.java
+++ Java/TurnOff.java
@@ -14,9 +14,13 @@ public class TurnOff {
 		double [][] cd_lt_lg = {{-23.3251,-46.3810},{-8.0314,-34.5252},{-3.0607,-60.0130}};
 		String [] fabrica ={"Brasília/DF"};
 		double []fab_lt_lg= {-15.4647,-47.5547};
+		String [][] cliente = {{"T-Systems","Rio Branco/AC"},{"FIAP","Andorinha/BA"},{"7Comm","Porto Alegre/RS"}};
+		double [][] cli_lt_lg={{-09.5829,-67.4836},{-10.2041,-39.5008},{-30.0159,-51.1348}};
+		String cli="";
 		double []vl_total= new double[3];
 		double soma=0, peso_total=0;
 		int[] qtd_prod= new int [3];
+		int[] qtd_prod_estoque= {50,50,50};
 		boolean resp=false;
 		NumberFormat df = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
 		DecimalFormat pf= new DecimalFormat("0.000");
@@ -40,13 +44,20 @@ public class TurnOff {
 			System.out.println("================================");
 		}
 		// Pedido
-		System.out.println("Nome do cliente: ");
-		String cliente = entrada.next();
+		System.out.println("Escolha o cliente que irá realizar o pedido \nOpções: \n 0 - T-System \n 1 - FIAP \n 2 - 7Comm");
+		int cliopt =entrada.nextInt();
 		
+		if(cliopt==0){
+			cli=cliente[0][0];
+		}if(cliopt==1){
+			cli=cliente[1][0];
+		}if(cliopt==2){
+			cli=cliente[2][0];
+		}
 		
 		//Pedido: Quantidade por modelo
 		do{
-		try{
+
 			
 		
 		//Laço para obter quantos beancos de cada cor serão "comprados"
@@ -67,14 +78,16 @@ public class TurnOff {
 			//Soma peso total do pedido
 			peso_total=peso_total+(qtd_prod[i]*beacon_prec_vol_pes[i][2]);
 		}
-		}catch(Exception e){
-			System.out.println(getExcecao(e));
+		
+		
+		
+		
 		
 		// Valida se existe produto no pedido
 		if(soma>0){
 		
 		//Instancia o método para retornar a distancia e Centro de distribuição mais próximo
-		double dist=centroMaisProximo(cd_lt_lg,cd_nome);
+		double dist=centroMaisProximo(cliopt,cli_lt_lg,cd_lt_lg,cd_nome);
 		System.out.println("Distância em km: "+Math.round(dist));
 		
 		
@@ -89,7 +102,7 @@ public class TurnOff {
 
 		//Teste de dados do pedido(Quantidade solicitado por produto, valor total dos produtos e peso total)	
 		System.out.println("===========Nota Fiscal===========");
-		System.out.println("Cliente: "+cliente);
+		System.out.println("Cliente: "+cli);
 		System.out.println("CPF/CNPJ: XXXXXXXXXX-XX");
 		System.out.println("Quantidade "+beacon_nome[0]+": "+qtd_prod[0]);
 		System.out.println("Quantidade "+beacon_nome[1]+": "+qtd_prod[1]);	
@@ -111,31 +124,22 @@ public class TurnOff {
 			}if(op=='N'){
 				resp=false;
 			}
-		}}finally{
-			System.out.println("Deseja tentar novamente?\n S = Sim \n N = Não");
-		char op = entrada.next().toUpperCase().charAt(0);
-		if(op=='S'){
-			resp=true;
-		}if(op=='N'){
-			resp=false;
-			
-		}
-			
 		}
 		}while(resp==true);
 	}
-
+	
+	
 	//Metodo para retornar o centro de distribuição mais próximo
-	public static double centroMaisProximo(double cd_lt_lg[][],String cd_nome[] ){
+	public static double centroMaisProximo(int cliopt, double cli_lt_lg[][],double cd_lt_lg[][],String cd_nome[] ){
 		double dist[]= new double[3];
 		double aux;
 		String auxcidade;
 		
 		
 		
-		//Faz a conta entre "CD" de Destino
+		//Faz a conta entre "CD" e Destino
 		for(int i=0;i<3;++i){
-			dist[i]= (Math.sqrt((-5.4742-cd_lt_lg[i][0])*(-5.4742-cd_lt_lg[i][0]) +(-35.124-cd_lt_lg[i][1])*(-35.124-cd_lt_lg[i][1])))*109;
+			dist[i]= (Math.sqrt((cli_lt_lg[cliopt][0]-cd_lt_lg[i][0])*(cli_lt_lg[cliopt][0]-cd_lt_lg[i][0]) +(cli_lt_lg[cliopt][1]-cd_lt_lg[i][1])*(cli_lt_lg[cliopt][1]-cd_lt_lg[i][1])))*109;
 			System.out.println("CD "+cd_nome[i]+"\nDistância: "+Math.round(dist[i])+" Km");
 		}
 		
