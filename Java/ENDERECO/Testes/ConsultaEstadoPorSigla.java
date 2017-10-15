492588598ffdea37c07ccdc9e4a30417a6c0d147
Vitor <vtorzy@gmail.com>
2017-10-15 17:24:35 -0200

    Create ConsultaEstadoPorSigla.java

---
+++ Java/ENDERECO/Testes/ConsultaEstadoPorSigla.java
@@ -0,0 +1,42 @@
+package br.com.fiap.testeCidade;
+
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import br.com.fiap.bean.Cidade;
+import br.com.fiap.bean.Estado;
+import br.com.fiap.bo.CidadeBO;
+import br.com.fiap.dao.CidadeDAO;
+
+public class ConsultaPorSigla {
+	public static void main(String[] args) {
+			CidadeDAO dao = null;
+		try {
+			dao = new CidadeDAO();
+//			List<Cidade> lista = CidadeBO.consultaPorSigla(JOptionPane.showInputDialog("SIGLA"));
+			List<Cidade> lista = dao.consultaPorSigla(JOptionPane.showInputDialog("SIGLA"));
+			
+			//Puxa os atributos de  Cidade primeiro
+			for (Cidade c : lista) {
+				System.out.println("CODIGO DA CIDADE:	" + c.getCd_cidade());
+				System.out.println("NOME DA CIDADE:		" + c.getNm_cidade());
+				//Puxa os atributos de Estado
+				for (Estado e : c.getListaEstado()) {
+					System.out.println("CODIGO DO ESTADO: 	" + e.getCd_estado());
+					System.out.println("NOME DO ESTADO: 	" + e.getNm_estado());
+					System.out.println("SIGLA DO ESTADO: 	" + e.getDs_sigla());
+					System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = ");
+				}
+			}				
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		} finally {
+			try {
+				dao.fechar();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
+}
