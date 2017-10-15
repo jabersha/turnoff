c1dd042d207e82f60a08a2cdaf4092726ec47975
Vitor <vtorzy@gmail.com>
2017-10-14 23:34:18 -0300

    Add files via upload

---
+++ Java/ENDERECO/Beans/Bairro.java
@@ -0,0 +1,57 @@
+package br.com.fiap.bean;
+
+import java.util.List;
+
+public class Bairro {
+	private int cd_bairro;
+	private String nm_bairro;
+	private int cd_cidade;
+//	private List<Cidade> listaCidade;
+
+	public Bairro() {
+
+	}
+	
+	public Bairro(int cd_bairro, String nm_bairro, int cd_cidade) {
+		super();
+		this.cd_bairro = cd_bairro;
+		this.nm_bairro = nm_bairro;
+//		this.listaCidade = listaCidade;
+	}
+
+//	public List<Cidade> getListaCidade() {
+//		return listaCidade;
+//	}
+//
+//
+//
+//
+//	public void setListaCidade(List<Cidade> listaCidade) {
+//		this.listaCidade = listaCidade;
+//	}
+
+	public int getCd_bairro() {
+		return cd_bairro;
+	}
+
+	public void setCd_bairro(int cd_bairro) {
+		this.cd_bairro = cd_bairro;
+	}
+	
+	public String getNm_bairro() {
+		return nm_bairro;
+	}
+
+	public void setNm_bairro(String nm_bairro) {
+		this.nm_bairro = nm_bairro;
+	}
+
+	public int getCd_cidade() {
+		return cd_cidade;
+	}
+
+	public void setCd_cidade(int cd_cidade) {
+		this.cd_cidade = cd_cidade;
+	}
+	
+}
