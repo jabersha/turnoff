115bc27587b068f4934105d12bc0fa602b30eadf
Vitor <vtorzy@gmail.com>
2017-10-15 20:49:47 -0200

    Add files via upload

--- Java/ENDERECO/BO/EstadoBO.java
+++ Java/ENDERECO/BO/EstadoBO.java
@@ -13,11 +13,10 @@ public class EstadoBO {
 		return msg;
 	}
 
-	public static Estado consultarPorCodigo(int c) throws Exception {
+	public static List<Estado> consultarPorCodigo(int c) throws Exception {
 		EstadoDAO dao = new EstadoDAO();
-		Estado e = dao.consultaPorCodigo(c);
+		List<Estado> e = dao.consultaPorCodigo(c);
 		dao.fechar();
-
 		return e;
 	}
 
@@ -25,22 +24,7 @@ public class EstadoBO {
 		EstadoDAO dao = new EstadoDAO();
 		List<Estado> e = dao.consultaPorSigla(s);
 		dao.fechar();
-
 		return e;
 	}
 
-	public static Estado excluir(int c) throws Exception {
-		EstadoDAO dao = new EstadoDAO();
-		Estado e = excluir(c);
-		dao.fechar();
-
-		return e;
-	}
-
-	public static String atualizar(Estado e) throws Exception {
-		EstadoDAO dao = new EstadoDAO();
-		String x = dao.atualizar(e);
-		dao.fechar();
-		return x;
-	}
 }
