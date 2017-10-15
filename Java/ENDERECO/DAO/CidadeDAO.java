8bd524d1f7ea487a2d06391d64ee9e112de29d81
Vitor <vtorzy@gmail.com>
2017-10-14 23:39:22 -0300

    Add files via upload

--- Java/ENDERECO/DAO/CidadeDAO.java
+++ Java/ENDERECO/DAO/CidadeDAO.java
@@ -19,60 +19,18 @@ public class CidadeDAO {
 		con.close();
 		return "Fechado";
 	}
-	
-//	public int RecuperarID()throws Exception{
-//
-//		PreparedStatement estrutura  = con.prepareStatement("SELECT SQ_ESTADO.NEXTVAL \"ID\" FROM SYS.DUAL");
-//
-//		ResultSet rs = estrutura.executeQuery();
-//
-//		int codigo = 0;
-//
-//		if(rs.next()){
-//			codigo = rs.getInt("ID");
-//		}
-//
-//		rs.close();
-//		estrutura.close();
-//		return codigo;
-//
-//	}
-	
+
 	public String insert(Cidade c) throws Exception {
 		
 		Estado e = new Estado();
-		
 		PreparedStatement ps = con.prepareStatement
-		("INSERT INTO T_VFC_ESTADO(cd_estado, nm_estado, ds_sigla)"
-				+"VALUES (?,?,?)");
-		ps.setInt(1, e.getCd_estado());
-		ps.setString(2, e.getNm_estado());
-		ps.setString(3, e.getDs_sigla());
-		ps = con.prepareStatement
-		("INSERT INTO T_VFC_CIDADE"
+		("INSERT INTO T_VFC_CIDADE (cd_cidade, nm_cidade, cd_estado)"
 		+"VALUES (?,?,?)");
 		ps.setInt(1, c.getCd_cidade());
 		ps.setString(2, c.getNm_cidade());
-		ps.setInt(3, e.getCd_estado());
+		ps.setInt(3, c.getCd_estado());
 		ps.execute();
-//		for (Estado e : c.getListaEstado()) {
-//			new EstadoDAO().insert(e, c.getCd_cidade());
-//		}
 		ps.close();
 		return "Inserido com sucesso!";
 	}
-//	public String insert(Cidade c) throws Exception {
-//		
-//		Estado e = new Estado();
-//		PreparedStatement ps = con.prepareStatement
-//		("INSERT INTO T_VFC_CIDADE (cd_cidade, nm_cidade, cd_estado)"
-//		+"VALUES (?,?,?)");
-//		ps.setInt(1, c.getCd_cidade());
-//		ps.setString(2, c.getNm_cidade());
-//		ps.setInt(3, c.getCd_estado());
-//		ps.execute();
-//		ps.close();
-//		return "Inserido com sucesso!";
-//	}
-	
 }
